2a46850e24192c97f5a1a3d807250203d72f6cdc
Radian <craziestdavid@gmail.com>
2017-01-03 20:36:41 +0800

    init

---
+++ Ashley.js
@@ -0,0 +1,9 @@
+window.Ashley = {
+  responseCallback : function (responseObject){
+    console.log(responseObject)
+
+  },
+  requestCallback : function (requestObject){
+    console.log(requestObject)
+  }
+}
