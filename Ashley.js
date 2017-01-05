ccd69d0de324d82f8e94fa5f8062e7ce255c9384
Ashley <lilis1224@gmail.com>
2017-01-05 16:37:12 +0800

    Commit basic file
    .scss .css and bg

--- Ashley.js
+++
@@ -1,9 +0,0 @@
-window.Ashley = {
-  responseCallback : function (responseObject){
-    console.log(responseObject)
-
-  },
-  requestCallback : function (requestObject){
-    console.log(requestObject)
-  }
-}
