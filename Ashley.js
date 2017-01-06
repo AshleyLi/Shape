23fcd34ba4314f8447a3b388e49322f82e5f3f52
AshleyLi <IUILab@10410.cs.nccu.edu.tw>
2017-01-06 14:36:25 +0800

    基礎資訊取得

--- Ashley.js
+++ Ashley.js
@@ -2,12 +2,12 @@ window.Ashley = {
   responseCallback : function (responseObject){
     // Get shape information
     console.log(responseObject)
-    console.log('Shape = ' + responseObject.result.segments[0].candidates[0].label)
 
     // Print basic shape information
     var shapeName = responseObject.result.segments[0].candidates[0].label;
     $(".js_shapeName").text(shapeName);
 
+    // 取得 PosX and PosY array
     var pointsX = [];
     var pointsY = [];
     for ( i = 0 ; i < 4; i++){
@@ -15,9 +15,24 @@ window.Ashley = {
         var pointY = Math.floor(responseObject.result.segments[0].candidates[0].primitives[i].firstPoint.y);
         pointsX[i] = pointX;
         pointsY[i] = pointY;
-
+        pointsX.sort();
+        pointsY.sort();
     }
-    console.log("pointsX = [" + pointsX + "] , pointsY = [" + pointsY + "]" );
+    // remove tha same value from PosX & PosY array and update array
+    pointsX = pointsX.filter(function(element, index, arr){
+      return arr.indexOf(element)=== index;
+    });
+    pointsY = pointsY.filter(function(element, index, arr){
+      return arr.indexOf(element)=== index;
+    });
+
+    //show information
+    $(".js_pointsX").text(pointsX);
+    $(".js_pointsY").text(pointsY);
+    $(".js_shapeW").text(pointsX[1]-pointsX[0]);
+    $(".js_shapeH").text(pointsY[1]-pointsY[0]);
+
+
 
   },
   requestCallback : function (requestObject){
