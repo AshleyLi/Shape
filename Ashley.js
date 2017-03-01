acedff18aae971683aca156280037a893cadab85
AshleyLi <IUILabiMac@10410.cs.nccu.edu.tw>
2017-03-01 23:07:56 +0800

    m of undo

--- Ashley.js
+++ Ashley.js
@@ -38,7 +38,7 @@ var tableX = [], tableY = []; // 形成t字兩線斷的 x and y
 var tolerance = 50; // T字判斷 線段偏離容許值
 var cellMidline; // cell的中線x位置
 
-var undoString = []; // 紀錄undo時要執行的：eventType類型、target目標、action動作、task執行項目
+var undoString = ""; // 紀錄undo時要執行的：eventType類型、target目標、action動作、task執行項目
 
 
 
@@ -238,7 +238,7 @@ window.Ashley = {
     // Suggestions checked =====================================================
     function checkedElement(e){
       var target = e;
-      $(target+".form-check:first-child input").attr("checked");
+      $(target+" .form-check:first-child input").attr("checked");
     }
     // Attr xkID  ==============================================================
     function addXkID(e){
@@ -248,18 +248,35 @@ window.Ashley = {
     }
     // Set undo function =======================================================
     // 紀錄undo時要執行的：eventType類型、target目標、action動作、task執行項目
-    function setUndoEvent (e, t0 ,a ,t1){
+    function undoEvent_set (e, t0 ,a ,t1){
       var eventType = e;
       var target = t0;
       var action = a;
       var task = t1;
-      console.log(eventType+","+target+","+action+","+task);
+
+      switch (eventType) {
+        case 'addClass':
+          console.log("Event = addClass, "+target+","+action+","+task);
+          break;
+        case 'addElenment':
+          console.log("Event = addClass, "+target+","+action+","+task);
+          break;
+        default:
+
+      }
+
+    }
+    function undoEvent_execute(){
+      eval(undoString);
+      undoString = "console.log('No undo event.')" ;
 
     }
 
     // Tableview ===============================================================
     // Create a tableview.
     function ATableview(){
+      currentID++;
+      undoString = "$('[xkID="+ currentID +"]').remove();";
       if(tableiQty != 1){
         $("#tableview").css({
           "display" : "initial",
@@ -290,25 +307,32 @@ window.Ashley = {
     }
     // drawing a rect on a cell.
     function ACellRectangle(){
+
       if (pointsX[0] < cellMidline) {
         // console.log("列表 左側的矩形");
         if( $(".icon.icon-f7").length == 0 ){
           $(".js_tableUl > .item-content").prepend("<div class='item-media'><i class='icon icon-f7'></i></div>");
+          undoString = "$('.js_tableUl > .item-content > .item-media').remove();";
         }
         clearCanvas();
       } else {
         // console.log("列表 右側的矩形"); = 加上「 > 」
         $(".item-content").addClass("item-link");
+        undoString = "$('item-content').removeClass('item-link');";
+
       }
       clearCanvas();
     }
     function ACellLine(){
+      currentID++;
 
       // 線段＠列表左側 及 右側
       if (pointsX[0] < cellMidline) {
         // console.log("列表 左側的線段");
         if( $(".item-title").is(':empty') && $(".subforCell").length == 0 ){
           $(".js_tableUl > .item-content > .item-inner > .item-title").append("Item title");
+          undoString("$('.js_tableUl > .item-content > .item-inner > .item-title .Item title').remove();");
+
         }else if( $(".item-title").length > 0 && $(".subforCell").length == 0){
           $(".js_tableUl > .item-content > .item-inner > .item-title").append("<div class='subforCell'>subtitle</div>");
         }else{
@@ -342,7 +366,8 @@ window.Ashley = {
     // Show rectangle suggestion ===============================================
 
     function ARectangle() {
-      setUndoEvent("eventType類型","target目標","action動作","task執行項目");
+      currentID++;
+      undoString = "$('[xkID="+ currentID +"]').remove();";
       checkedElement(".popover-title");
       if(rectW >= rectH){
         if(rectH > unitH){
@@ -391,20 +416,15 @@ window.Ashley = {
 
     // Show Line suggestion ====================================================
     function ALine() {
-
-      // Greate the first component
-      $(".identification").append("<span xkID='"+ currentID + "' style='width:"+lineW +"px;left:"+pointsX[0]+"px;top:"+pointsY[0] +"px;text-overflow:ellipsis; white-space: nowrap; overflow:hidden; font-size:20px; ;'>今、明兩天天氣不穩定，有局部較大雨勢發生的機率；今日鋒面接近，臺灣中部以北地區及澎湖、金門、馬祖有短暫陣雨或雷雨，東半部地區亦有局部短暫陣雨，南部地區為短暫陣雨後多雲；明日鋒面通過及大陸冷氣團南下，各地氣溫下降；臺灣北部、東北部地區及金門、馬祖有陣雨或雷雨，中部、東部、東南部地區及澎湖有短暫陣雨，其他地區亦有局部短暫陣雨。今、明兩天金門、馬祖易有局部霧或低雲影響能見度，請注意。</span>");
-
-      // add the first suggeted conponent.
-      $(".identification").append("<span xkID='"+ currentID + "' style='width:"+lineW +"px;left:"+pointsX[0]+"px;top:"+pointsY[0] +"px;height:23px; text-overflow:ellipsis; white-space: nowrap; overflow:hidden; font-size:20px; ;'>今、明兩天天氣不穩定，有局部較大雨勢發生的機率；今日鋒面接近，臺灣中部以北地區及澎湖、金門、馬祖有短暫陣雨或雷雨，東半部地區亦有局部短暫陣雨，南部地區為短暫陣雨後多雲；明日鋒面通過及大陸冷氣團南下，各地氣溫下降；臺灣北部、東北部地區及金門、馬祖有陣雨或雷雨，中部、東部、東南部地區及澎湖有短暫陣雨，其他地區亦有局部短暫陣雨。今、明兩天金門、馬祖易有局部霧或低雲影響能見度，請注意。</span>");
+      currentID++;
+      // Create the first component & set undo event
+      $(".identification").append("<span xkID='"+ currentID+"' style='width:"+lineW +"px;left:"+pointsX[0]+"px;top:"+pointsY[0] +"px;text-overflow:ellipsis; white-space: nowrap; overflow:hidden; font-size:20px; ;'>今、明兩天天氣不穩定，有局部較大雨勢發生的機率；今日鋒面接近，臺灣中部以北地區及澎湖、金門、馬祖有短暫陣雨或雷雨，東半部地區亦有局部短暫陣雨，南部地區為短暫陣雨後多雲；明日鋒面通過及大陸冷氣團南下，各地氣溫下降；臺灣北部、東北部地區及金門、馬祖有陣雨或雷雨，中部、東部、東南部地區及澎湖有短暫陣雨，其他地區亦有局部短暫陣雨。今、明兩天金門、馬祖易有局部霧或低雲影響能見度，請注意。</span>");
+      undoString = "$('[xkID="+ currentID +"]').remove();";
 
       // open & set suggestions pop positionX,Y
       $('.js_line').css({"display" : "initial"});
+      checkedElement(".js_line");
       $(".js_suggestions.regular").css({"display":"initial","left": pointsX[1]+10 + "px","top": pointsY[0] + shift -$(".js_suggestions").height()/2 + "px"});
-
-      // show the shape basic information
-
-
     }
 
     // Draw popview elements  ==================================================
@@ -571,20 +591,22 @@ window.Ashley = {
   }
 }
 
+// UNDO ========================================================================
+$(document).on("click",".js_undo",function(){
+  eval(undoString);
+});
 
 // AFTER CONFIRM ===============================================================
 $(document).on("click",".js_confirmType",function(){
-    // Close suggestions
+    // Close suggestions pop
     $('.popover-content > div').css({"display" : "none"});
     $('.js_suggestions').css({"display":"none"});
-
-    // Clear the canvas.
+    // Clear the stroke canvas.
     $("paper-fab[icon='delete']").trigger("click");
-
-    // Show the counting result
+    // Show the indexP result display
     $(".js_showCount").css("display","initial");
 
-    // Get and show the selected value and show
+    // Show the indexP result
     var indexP = parseInt($( "input:checked" ).attr("indexP"));
     $(".score").append( "<li>#" + shapeQty + " —— " + indexP + "p</li>" )
 
@@ -748,8 +770,37 @@ $(document).on("click",".js_confirmType",function(){
     $(".js_percentage").text(percentage + "%");
 
 });
+// table cell
+$(document).on("click",".js_confirmCell",function(){
+  // Close suggestions pop
+  $('.popover-content > div').css({"display" : "none"});
+  $('.js_suggestions').css({"display":"none"});
+  // Clear the stroke canvas.
+  $("paper-fab[icon='delete']").trigger("click");
 
+  $(".item-after").empty();
+  var elementType = $(".js_cellConponents input:checked ").val();
 
+  switch (elementType) {
+      case 'Text':
+        $(".item-after").append(elementType);
+        break;
+      case 'Text >':
+        $(".item-after").append(elementType);
+        break;
+      case 'Text input':
+        $(".item-after").append(elementType);
+        break;
+      case 'Check icon':
+        $(".item-after").append(elementType);
+        break;
+      case 'Switch':
+        $(".item-after").append(elementType);
+        break;
+    default:
+
+  }
+});
 // Popup =======================================================================
 
 $(document).on("click",".js_funcPop",function(){
@@ -759,4 +810,13 @@ $(document).on("click",".js_funcPop",function(){
   // Down the true elements layer of basic & up the myScript canvas.
   // The z-index of layers: 1.toolbar > 2.myScript camvas > 3.popup view > 4.mask > 5.basic elements
 
+});
+
+// Reload the page ========================================================================
+$(document).on("click",".js_clear",function(){
+  var txt;
+  var r = confirm("是否清除畫布所有元件？");
+  if (r == true) {
+      location.reload();
+  }
 });
