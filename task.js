4ba5520c38a4bd1b5e3b76aaa26a84a53e87449d
Ashley <lilis1224@gmail.com>
2017-03-24 00:30:53 +0800

    finished the basic task setting

--- task.js
+++ task.js
@@ -1,12 +1,14 @@
-var currentTask = 1; // 目前任務的順序
-var comletedTaskQty = 0; // 目前完成的任務數量  
+var currentTask = 0; // 目前任務的順序
+var comletedTaskQty = 0; // 目前完成的任務數量
+var taskIndex = []; //
 //TaskController================================================================
 $(document).on("click",".gotoNextTask",function(){
   var r = confirm("是否進入下一任務？");
   if (r == true) {
     $(".identification").empty();
     currentTask++;
-    addTaskElements(currentTask);
+    addTaskElements(taskIndex[currentTask]);
+    $(".js_taskNum").text(currentTask+1);
   }
   // 確認
 
@@ -17,17 +19,16 @@ $(document).on("click",".gotoNextTask",function(){
 $( document ).ready(function() {
   // 取得總長25的數列 ===================
   var arr = [];
-  var res = [];
   for (var i = 0; i < 25; i++) {
     arr[i] = i;
   }
   // 打亂數列的排序 ===================
   for (var i = 0, len = arr.length; i < len; i++) {
     var j = Math.floor(Math.random() * arr.length);
-    res[i] = arr[j];
+    taskIndex[i] = arr[j];
     arr.splice(j, 1);
   }
-  addTaskElements(currentTask);
+  addTaskElements(taskIndex[currentTask]);
 });
 
 
