4367a9118923f4031c78751e6faf8156bec8ac81
AshleyLi <IUILabiMac@10410.cs.nccu.edu.tw>
2017-03-28 22:35:17 +0800

    Revert "Array2CSV done."
    This reverts commit fdf405796076c17d47d603f3e474295377ca9b34.
    
    # Conflicts:
    #	task.js

--- task.js
+++ task.js
@@ -2,7 +2,38 @@ var currentTask = 0; // 任務順序
 var comletedTaskQty = 0; // 目前完成的任務數量
 var taskIndex = []; //
 var totalTask = 3;
-var arrData =[]; // User behavior storage
+
+//TaskController================================================================
+$(document).on("click",".gotoNextTask",function(){
+  // 任務已經全部完成，則開始儲存.csv
+  if(currentTask+1 == totalTask){
+    alert("儲存測試資料");
+
+    var test_array = [["name1", 2, 3], ["name2", 4, 5], ["name3", 6, 7], ["name4", 8, 9], ["name5", 10, 11]];
+  	var fname = "IJGResults";
+
+  	var csvContent = "data:text/csv;charset=utf-8,";
+  		arrData.forEach(function(infoArray, index){
+  			dataString = infoArray.join(",");
+  			csvContent += index < infoArray.length ? dataString+ "\n" : dataString;
+  		});
+  		var encodedUri = encodeURI(csvContent);
+  		window.open(encodedUri);
+
+
+
+
+  }else {
+  // 任務尚未結束，前往下一任務
+    var r = confirm("是否進入任務"+(currentTask+2)+"？");
+    if (r == true ) {
+      $(".identification").empty();
+      currentTask++;
+      addTaskElements(taskIndex[currentTask]);
+      $(".js_taskNum").text(currentTask+1);
+      arrData.push(timeIndex()+"Index"+currentTask+"/Task"+taskIndex[currentTask]+"." );
+    }
+  }
 
 
 
